0f1f68a8d7ac0ad2d0f8200cbcb716bc109117ca
Nuray <nuraynurzhaskyzy@gmail.com>
2024-09-04 11:47:21 +0500

    add User service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/database/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity) private repo: Repository<UserEntity>,
+  ) {}
+
+  async removeFlagsandCount() {
+    try {
+      const [trueish, count] = await this.repo.findAndCount({
+        where: { problem: true },
+      });
+      await this.repo
+        .createQueryBuilder()
+        .update()
+        .set({ problem: false })
+        .where('problem = :problem', { problem: true })
+        .execute();
+      const now = await this.repo.find({ where: { problem: true } });
+      const res = {
+        beforeHadProblem: count,
+        nowUsersWithProblem: now,
+      };
+      return res;
+    } catch (error) {
+      console.log('Error with removeing flag: ', error);
+      throw new HttpException(
+        'Server-side Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
